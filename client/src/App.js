e299033d8a2a16cfed6df387ea2798e68e519cd5
oliverkp123 <kochpaizoliver@gmail.com>
2024-03-13 22:48:40 -0400

    Basic File Layout for Development

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './pages/Home';
+import Schedule from './pages/Schedule';
+import Rank from './pages/Rank';
+import Registration from './pages/Registration';
+
+import MeetingRequest from './pages/MeetingRequest';
+
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Router>
+        <div className="navbar">
+          <Link to="/"> Home </Link>
+          <Link to="/schedule"> Schedule </Link>
+          <Link to="/rank"> Rank </Link>
+          <Link to="/meetingRequest"> Meeting Request </Link>
+        </div>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/schedule" element={ <Schedule /> } />
+          <Route path="/rank" element={ <Rank /> } />
+          <Route path="/meetingRequest" element={ <MeetingRequest /> } />
+          <Route path="/register" element={ <Registration /> } />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
