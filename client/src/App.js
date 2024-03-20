c18272d65ac7226a83ccf29a242225e5db6a66ef
oliverkp123 <kochpaizoliver@gmail.com>
2024-03-20 13:17:01 -0400

    Could not pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from './pages/Home';
-import Schedule from './pages/Schedule';
-import Rank from './pages/Rank';
-import Registration from './pages/Registration';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Schedule from "./pages/Schedule";
+import Rank from "./pages/Rank";
+import Registration from "./pages/Registration";
+import {Settings} from "./pages/UserSettings";
 
-import MeetingRequest from './pages/MeetingRequest';
 
-import {NavbarComponent} from './components/Navbar'
+import MeetingRequest from "./pages/MeetingRequest";
 
+import { NavbarComponent } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
-          <NavbarComponent />
-        
+        <NavbarComponent />
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -24,6 +23,7 @@ function App() {
           <Route path="/rank" element={<Rank />} />
           <Route path="/meetingRequest" element={<MeetingRequest />} />
           <Route path="/registration" element={<Registration />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </Router>
     </div>
