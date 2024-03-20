f939de448dd4ba8748083845720eec0be55f488a
oliverkp123 <kochpaizoliver@gmail.com>
2024-03-20 13:32:07 -0400

    Adding coverage report and enhancing test script

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { Users } = require('../models');
-const bcrypt = require('bcrypt');
 const { authRegister } = require('../controllers/Users');
 
 
