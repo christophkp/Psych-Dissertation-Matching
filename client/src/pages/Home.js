00f714c603dc0513f573048f07123d1dc9a26af9
oliverkp123 <kochpaizoliver@gmail.com>
2024-03-25 22:36:52 -0400

    Redesigned Home #4

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import img from "../assets/testimage.jpeg"
+import img from "../assets/pfp.png"
 
 function Home() {
   const [listofFaculty, setListOfFaculty] = useState([]);
@@ -18,30 +18,25 @@ function Home() {
         <h1 className="text-center mt-4" style={{ textTransform: 'uppercase', color: '#007b5e' }}>Faculty</h1>
         {listofFaculty.map((value, key) => {
           return(
-            <div key={key} className="flip-card col-md-3 mt-4 mb-3" >
-              <div className="flip-card-inner border border-success" >
-                <div className="flip-card-front">
-                  <div className="card border-0">
-                    <div className="card-body p-0">
-                      <img className="img-fluid" src={img} alt="card" />
-                      <h4 className="card-title mt-3">{value.firstName} {value.lastName}</h4>
-                      <button className="btn btn-success"><i className="bi bi-arrow-repeat"></i></button>
-                    </div>
-                  </div>
+            <div key={key} className="col-md-3 mt-4 mb-3" >
+              <div className="card border border-success" style={{maxHeight: "250px", overflowY: "auto"}}>
+                <div className="d-flex justify-content-center align-items-center">
+                  <img src={img} className="card-img-top rounded-circle mt-3" style={{ width: "100px" }} alt="card" />
                 </div>
-                <div className="flip-card-back">
-                  <div className="card border-0">
-                    <div className="card-body" style={{ maxHeight: "300px", overflowY: "auto" }}>
-                      <h5>Information:</h5>
-                      <p>{value.information}</p>
-                      <h5>Research:</h5>
-                      <p>{value.research}</p>
+                <div className="card-body p-0 text-center">
+                  <div style={{ position: "sticky", top: 0, backgroundColor: "#fff", zIndex: 1 }}>
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+                      <h4 className="card-title mt-3 mb-4 text-center"><strong>{value.firstName} {value.lastName}</strong></h4>
+                      <hr style={{ width: "50%", margin: "0 auto" }} />
                     </div>
-                  </div>
+                  </div> 
+                  <h6 className="mt-4 border border-success rounded" style={{ padding: "5px", display: "inline-block" }}>Information </h6>
+                  <p>{value.information}</p>
+                  <h6 className=" border border-success rounded" style={{ padding: "5px", display: "inline-block" }}>Research </h6>
+                  <p>{value.research}</p>
                 </div>
               </div>
             </div>
-            
           )
         })}
       </div>
