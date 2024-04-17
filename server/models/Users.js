383e9a2848f944810de221035ec9d632d8c5cd96
christophkp <kochpaizchristoph@gmail.com>
2024-04-17 17:25:08 -0400

    format fix

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,59 +1,56 @@
 module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define("Users", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "student",
+    },
+    information: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    research: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    profilepic: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "defaultprofile.png",
+    },
+  });
 
-    const Users = sequelize.define("Users", {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: "student",
-      },
-      information: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      research: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      profilepic: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: "defaultprofile.png",
-      },
+  Users.associate = (models) => {
+    Users.hasMany(models.Meetings, {
+      foreignKey: "studentId",
+      onDelete: "cascade",
     });
+    Users.hasMany(models.Meetings, {
+      foreignKey: "facultyId",
+      onDelete: "cascade",
+    });
+    Users.hasMany(models.Ranks, {
+      foreignKey: "rankerId",
+      onDelete: "cascade",
+    });
+  };
 
-    Users.associate = (models) => {
-      Users.hasMany(models.Meetings, {
-        foreignKey: 'studentId', 
-        onDelete: "cascade",
-      });
-      Users.hasMany(models.Meetings, {
-        foreignKey: 'facultyId', 
-        onDelete: "cascade",
-      });
-      Users.hasMany(models.Ranks, {
-        foreignKey: 'rankerId', 
-        onDelete: "cascade",
-      });
-    };
-
-
-
-    return Users;
-}
+  return Users;
+};
